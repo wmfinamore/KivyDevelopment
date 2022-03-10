6ed04e367b2c890725545173bb1364fa75531821
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-03-09 22:18:30 -0300

    Simple app, using buttons for example

---
+++ main.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+# interface
+class Interface(Widget):
+    pass
+
+
+# app creation
+class TestApp(App):
+    pass
+
+
+TestApp().run()
