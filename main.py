f76a3abc61857498c81c21aa2b001b46c3bc240e
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-03-13 15:30:21 -0300

    Example for use FloatLayout

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from kivy.app import App
-from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
 
 
 # interface
-class Interface(AnchorLayout):
-    pass
+class Inti(FloatLayout):
+    layout = BoxLayout()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        b1 = Button(text="Hello", size_hint=(.25, .25), pos=(200, 100))
+        self.layout.add_widget(b1)
+        self.add_widget(self.layout)
 
 
 # app creation
