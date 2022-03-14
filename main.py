cbdf4dd5d339a94c4b827ffbcf7b71589860d297
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-03-14 16:57:25 -0300

    Example for use StackLayout

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.stacklayout import StackLayout
 
 
 # interface
 class Inti(FloatLayout):
-    layout = BoxLayout()
+    pass
 
+
+class stackInterface(StackLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        b1 = Button(text="Hello", size_hint=(.25, .25), pos=(200, 100))
-        self.layout.add_widget(b1)
-        self.add_widget(self.layout)
+        for i in range(30):
+            b1 = Button(text=str(i+1), size_hint=(.15, .15), background_color=(i*0.1, i*0.2, i*0.3, 1))
+            self.add_widget(b1)
 
 
 # app creation
